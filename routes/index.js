ede88cef9a51b4dbe78ee114c0dd16db98ba703f
Dariusz Okonski <darek200180@gmail.com>
2020-03-14 21:07:56 +0000

    add routes get/cars/new and post/cars

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,16 @@ router.get('/cars', (req, res) => {
   res.render('cars');
 })
 
+router.get('/cars/new', (req, res) => {
+  res.render('new');
+})
+
+router.post('/cars', (req, res) => {
+  console.log(req.body.car);
+
+  res.send('You will add a new car');
+});
+
 
 
 module.exports = router;

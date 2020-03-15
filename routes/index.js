2577d73b80736143297c46e10bbcf99b5ec85028
Dariusz Okonski <darek200180@gmail.com>
2020-03-15 08:11:12 +0000

    add routes for edit car

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Car = require('../models/car');
 
+// home page
 router.get('/', (req, res) => {
   res.render('home');
 });
 
+// show all cars
 router.get('/cars', (req, res) => {
   Car.find({}, (err, cars) => {
     if (err) {
@@ -18,6 +20,7 @@ router.get('/cars', (req, res) => {
   })
 })
 
+// add new car
 router.get('/cars/new', (req, res) => {
   res.render('new');
 })
@@ -33,6 +36,28 @@ router.post('/cars', (req, res) => {
   })
 });
 
+// edit designated car
+router.get('/cars/:id/edit', (req, res) => {
+  Car.findById(req.params.id, (err, foundCar) => {
+    if (err) {
+      res.redirect('/cars')
+    } else {
+      res.render('edit', {
+        car: foundCar
+      })
+    }
+  })
+})
+
+router.put('/cars/:id', (req, res) => {
+  Car.findByIdAndUpdate(req.params.id, req.body.car, (err, updatedCar) => {
+    if (err) {
+      res.redirect('/');
+    } else {
+      res.redirect('/cars');
+    }
+  });
+})
 
 
 module.exports = router;
